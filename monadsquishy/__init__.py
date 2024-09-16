21ec7a0985c6ecf7f3d760480fadd6ecf351b0fd
SothanaV <nai6320@gmail.com>
2024-09-16 13:20:09 +0700

    [UPDATE] setup and __init__

--- monadsquishy/__init__.py
+++ monadsquishy/__init__.py
@@ -1,3 +1,11 @@
 # monadsquishy/__init__.py
+
+__title__ = 'DigitalStoreMesh Services'
+__version__ = '0.1.4'
+__author__ = 'Wasit Limprasert'
+__license__ = 'MIT'
+
+VERSION = __version__
+
 from . import functions as sf
 from .squishy import Squishy
--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
+import os
+import re
 from setuptools import setup, find_packages
 
+package="monadsquishy"
+
+def get_version():
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
 setup(
-    name="monadsquishy",
-    version="0.1.3",
+    name=package,
+    version=get_version(),
     description="A Python package for squishy data transformation and monads",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
