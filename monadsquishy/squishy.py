7f4ab550f89e161e446bb13a2919b1bc07ec2094
adeenun <adeenun@storemesh.com>
2025-09-08 13:21:52 +0700

    [FIXED] delete pritn

--- monadsquishy/squishy.py
+++ monadsquishy/squishy.py
@@ -293,7 +293,6 @@ class Squishy:
                     _df_out_col.append(out_col)
                     # if funcs[-1].decorator_name != 'passed': # Check last function is not consistency auto add end function
                     if not funcs or getattr(funcs[-1], "decorator_name", None) != "passed":
-                        print(funcs[-1].decorator_name)
                         def end(x):
                             return x
                         funcs.append(end)
